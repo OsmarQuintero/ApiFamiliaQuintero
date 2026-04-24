ef4a6e932ac9b7368cf980257ebb48bb3067ea12
OsmarQuintero <quinterovaldzosmar.4@gmail.com>
2026-04-24 17:18:27 -0600

    Nueva Deploy

--- demo/src/main/java/com/tuapp/backend/repository/IntegranteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tuapp.backend.repository;
-
-import com.tuapp.backend.model.Integrante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IntegranteRepository extends JpaRepository<Integrante, Long> {
-}
